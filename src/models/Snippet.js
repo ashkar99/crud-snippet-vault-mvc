c929baeec8fc6e776a065dbd8e4a141a89ec24a7
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-16 21:49:37 +0100

    Defined Snippet schema

---
+++ src/models/Snippet.js
@@ -0,0 +1,26 @@
+/**
+ * Snippet Mongoose Model
+ * Represents a code snippet in the database.
+ */
+
+import mongoose from 'mongoose'
+
+// Define the schema
+const snippetSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required.'],
+    trim: true,
+    maxlength: [100, 'Title cannot exceed 100 characters.']
+  },
+  content: {
+    type: String,
+    required: [true, 'Content is required.'],
+    minlength: [1, 'Snippet cannot be empty.'],
+  }
+}, {
+  timestamps: true,
+  versionKey: false
+})
+
+export const Snippet = mongoose.model('Snippet', snippetSchema)
