68a866e090f037816238e86c9fea7f88e54b5968
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-25 15:31:45 +0100

    Mapped login and logout routes

--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -8,7 +8,10 @@ export const router = express.Router()
 
 const controller = new AccountController()
 
-// Map the GET request to the controller
-router.get('/register', (req, res, next) => controller.register(req, res, next))
 router.get('/register', (req, res, next) => controller.register(req, res, next))
 router.post('/register', (req, res, next) => controller.registerPost(req, res, next))
+
+router.get('/login', (req, res, next) => controller.login(req, res, next))
+router.post('/login', (req, res, next) => controller.loginPost(req, res, next))
+
+router.get('/logout', (req, res, next) => controller.logout(req, res, next))
