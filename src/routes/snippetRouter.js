b2b21685686441f98414982defe9d427b62a7dfa
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-25 16:16:23 +0100

    View rendering routing setup

--- src/routes/snippetRouter.js
+++ src/routes/snippetRouter.js
@@ -3,8 +3,14 @@
  */
 import express from 'express'
 import { authorizeLoggedOn } from '../middlewares/auth.js'
+import { SnippetController } from '../controllers/SnippetController.js'
 
 export const router = express.Router()
+const controller = new SnippetController()
+
+router.get('/', (req, res, next) => controller.index(req, res, next))
 
 router.get('/create', authorizeLoggedOn, (req, res) => res.send('Create Snippet Form'))
 router.post('/create', authorizeLoggedOn, (req, res) => res.send('Snippet Created'))
+
+router.get('/:id', (req, res, next) => controller.show(req, res, next))
