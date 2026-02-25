63f7e704833346044b2df677401cad600edad532
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-25 16:12:20 +0100

    Data formatting

--- src/controllers/SnippetController.js
+++ src/controllers/SnippetController.js
@@ -8,10 +8,13 @@ export class SnippetController {
   /**
    * Displays a list of all snippets.
    */
-  async index(req, res, next) {
+  async index (req, res, next) {
     try {
-      const snippets = await Snippet.find()
-      res.render('snippets/index', { snippets })
+      const viewData = {
+        snippets: (await Snippet.find().populate('author', 'username').sort({ createdAt: -1 }))
+          .map(snippetDoc => snippetDoc.toObject())
+      }
+      res.render('snippets/index', { viewData })
     } catch (error) {
       next(error)
     }
@@ -20,17 +23,21 @@ export class SnippetController {
   /**
    * Displays a specific snippet.
    */
-  async show(req, res, next) {
+  async show (req, res, next) {
     try {
-      const snippet = await Snippet.findById(req.params.id)
-
-      if (!snippet) {
+      const snippetDoc = await Snippet.findById(req.params.id).populate('author', 'username')
+      
+      if (!snippetDoc) {
         const error = new Error('The snippet you requested does not exist.')
         error.status = 404
         return next(error)
       }
 
-      res.render('snippets/show', { snippet })
+      const viewData = {
+        snippet: snippetDoc.toObject()
+      }
+
+      res.render('snippets/show', { viewData })
     } catch (error) {
       next(error)
     }
