caafc068a7eeadcec22e1358302305c60958cfdb
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-25 16:10:23 +0100

    Fetch single snippet by ID

--- src/controllers/SnippetController.js
+++ src/controllers/SnippetController.js
@@ -8,10 +8,29 @@ export class SnippetController {
   /**
    * Displays a list of all snippets.
    */
-  async index (req, res, next) {
+  async index(req, res, next) {
     try {
       const snippets = await Snippet.find()
       res.render('snippets/index', { snippets })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Displays a specific snippet.
+   */
+  async show(req, res, next) {
+    try {
+      const snippet = await Snippet.findById(req.params.id)
+
+      if (!snippet) {
+        const error = new Error('The snippet you requested does not exist.')
+        error.status = 404
+        return next(error)
+      }
+
+      res.render('snippets/show', { snippet })
     } catch (error) {
       next(error)
     }
