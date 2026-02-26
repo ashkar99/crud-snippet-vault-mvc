ffb7a036bc7533e74069a315bd06dfb6fc0aad3a
Sadek Alashkar <sa225sd@student.lnu.se>
2026-02-26 21:55:19 +0100

    Update and Delete routing with Ownership Middleware

--- src/routes/snippetRouter.js
+++ src/routes/snippetRouter.js
@@ -2,7 +2,7 @@
  * Snippet Routes
  */
 import express from 'express'
-import { authorizeLoggedOn } from '../middlewares/auth.js'
+import { authorizeLoggedOn, authorizeOwnership } from '../middlewares/auth.js'
 import { SnippetController } from '../controllers/SnippetController.js'
 
 export const router = express.Router()
@@ -14,3 +14,8 @@ router.get('/create', authorizeLoggedOn, (req, res, next) => controller.create(r
 router.post('/create', authorizeLoggedOn, (req, res, next) => controller.createPost(req, res, next))
 
 router.get('/:id', (req, res, next) => controller.show(req, res, next))
+router.get('/:id/update', authorizeLoggedOn, authorizeOwnership, (req, res, next) => controller.update(req, res, next))
+router.post('/:id/update', authorizeLoggedOn, authorizeOwnership, (req, res, next) => controller.updatePost(req, res, next))
+
+router.get('/:id/delete', authorizeLoggedOn, authorizeOwnership, (req, res, next) => controller.delete(req, res, next))
+router.post('/:id/delete', authorizeLoggedOn, authorizeOwnership, (req, res, next) => controller.deletePost(req, res, next))
